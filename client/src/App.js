88bd25be12e05a17fc04ccead8e9f59a041df3b2
it21291364 <it21291364@my.sliit.lk>
2024-12-01 13:36:37 +0530

    add comments to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FileUploadForm from './components/FileUploadForm';
-import LoadingPage from './components/LoadingPage';
-import ReviewResults from './components/ReviewResults';
-import DownloadResults from './components/DownloadResults';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FileUploadForm from "./components/FileUploadForm";
+import LoadingPage from "./components/LoadingPage";
+import ReviewResults from "./components/ReviewResults";
+import DownloadResults from "./components/DownloadResults";
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Define the route for the file upload form */}
+        <Route path="/" element={<FileUploadForm />} />
 
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<FileUploadForm/>} />
-                <Route path="/loading" element={<LoadingPage />} />
-                <Route path="/review" element={<ReviewResults />} />
-                <Route path="/download" element={<DownloadResults />} />
-            </Routes>
-           
-        </Router>
-    );
+        {/* Define the route for the loading page */}
+        <Route path="/loading" element={<LoadingPage />} />
+
+        {/* Define the route for reviewing results */}
+        <Route path="/review" element={<ReviewResults />} />
+
+        {/* Define the route for downloading results */}
+        <Route path="/download" element={<DownloadResults />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

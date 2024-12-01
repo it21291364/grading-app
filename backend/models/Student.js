6b6b459d0a531f99394eeed8fdd5093c7bf7fb2a
it21291364 <it21291364@my.sliit.lk>
2024-12-01 11:43:57 +0530

    add comments

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,6 +1,16 @@
-// models/Student.js
 const mongoose = require('mongoose');
 
+/**
+ * @typedef {Object} StudentSchema
+ * @property {String} studentId - The unique identifier for the student.
+ * @property {String} moduleCode - The code of the module the student is enrolled in.
+ * @property {Array.<Object>} answers - An array of answer objects.
+ * @property {Number} answers.questionNo - The question number.
+ * @property {String} answers.studentAnswer - The student's answer to the question.
+ * @property {Number} answers.studentMarks - The marks awarded for the student's answer.
+ * @property {String} answers.feedback - The feedback provided for the student's answer.
+ * @property {Number} totalMarks - The total marks obtained by the student.
+ */
 const StudentSchema = new mongoose.Schema({
   studentId: String,
   moduleCode: String,

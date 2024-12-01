0a00bc361cfc806273fb4e8bcc20e3da34002dca
it21291364 <it21291364@my.sliit.lk>
2024-12-01 11:32:26 +0530

    add comments.

--- backend/middleware/multerConfig.js
+++ backend/middleware/multerConfig.js
@@ -1,6 +1,9 @@
-// middleware/multerConfig.js
 const multer = require('multer');
 
+/**
+ * Configures multer to use memory storage.
+ * Files will be stored in memory as Buffer objects.
+ */
 const storage = multer.memoryStorage();
 
 const upload = multer({ storage });

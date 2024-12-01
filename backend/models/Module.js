d60f1b517c2cad031446629c6ce3a35968b83f30
it21291364 <it21291364@my.sliit.lk>
2024-12-01 11:35:53 +0530

    add comments to file.

--- backend/models/Module.js
+++ backend/models/Module.js
@@ -1,6 +1,23 @@
-// models/Module.js
 const mongoose = require('mongoose');
 
+/**
+ * @typedef {Object} Question
+ * @property {number} questionNo - The number of the question.
+ * @property {string} question - The text of the question.
+ * @property {string} expectedAnswer - The expected answer for the question.
+ * @property {string} instruction - Instructions for the question.
+ * @property {number} allocatedMarks - The marks allocated for the question.
+ */
+
+/**
+ * @typedef {Object} ModuleSchema
+ * @property {string} moduleName - The name of the module.
+ * @property {string} moduleCode - The code of the module.
+ * @property {string} batch - The batch associated with the module.
+ * @property {string} academicYear - The academic year of the module.
+ * @property {string} semester - The semester of the module.
+ * @property {Question[]} questions - The list of questions in the module.
+ */
 const ModuleSchema = new mongoose.Schema({
   moduleName: String,
   moduleCode: String,
